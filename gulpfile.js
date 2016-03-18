c6ecf35037253d436c4b9c513631ef3ac973c260
Jon Nardine <student@epicodus.com>
2016-03-18 12:55:22 -0700

    add api links including number of followers, number of followees, and account creation date

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,10 @@ var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var del = require('del');
 var jshint = require('gulp-jshint');
+var buildProduction = utilities.env.production;
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var lib = require('bower-files')({
   "overrides":{
     "bootstrap" : {
@@ -17,10 +21,8 @@ var lib = require('bower-files')({
     }
   }
 });
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var buildProduction = utilities.env.production;
+
+
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
@@ -28,21 +30,32 @@ gulp.task('concatInterface', function() {
   .pipe(gulp.dest('./tmp'));
 });
 
-gulp.task('jsBrowserify', ['concatInterface'], function() {
-  return browserify({entries: ['./tmp/allConcat.js'] })
-  .bundle()
-  .pipe(source('app.js'))
-  .pipe(gulp.dest('./build/js'));
+gulp.task('jshint', function (){
+  return gulp.src(['js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('minifyScripts', ['jsBrowserify'], function() {
-  return gulp.src('./build/js/app.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('./build/js'));
+gulp.task('jsBrowserify', ['concatInterface', 'jshint'] , function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
 });
 
-gulp.task("build", ['clean'], function() {
-  if (buildProduction) {
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("clean", function (){
+  return del(['build', 'tmp']);
+});
+
+gulp.task("build", ['clean'], function(){
+  if(buildProduction) {
     gulp.start('minifyScripts');
   } else {
     gulp.start('jsBrowserify');
@@ -51,30 +64,20 @@ gulp.task("build", ['clean'], function() {
   gulp.start('cssBuild');
 });
 
-gulp.task("clean", function() {
-  return del(['build', 'tmp']);
-});
-
-gulp.task('jshint', function() {
-  return gulp.src(['js/*.js'])
-  .pipe(jshint())
-  .pipe(jshint.reporter('default'));
-});
-
-gulp.task('bowerJS', function() {
+gulp.task('bowerJS', function (){
   return gulp.src(lib.ext('js').files)
-  .pipe(concat('vendor.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('./build/js'));
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
 });
 
-gulp.task('bowerCSS', function() {
+gulp.task('bowerCSS', function () {
   return gulp.src(lib.ext('css').files)
-  .pipe(concat('vendor.css'))
-  .pipe(gulp.dest('./build/css'));
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
 });
 
-gulp.task('bower', ['bowerJS', 'bowerCSS', 'cssBuild']);
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
 
 gulp.task('serve', function() {
   browserSync.init({
@@ -86,26 +89,27 @@ gulp.task('serve', function() {
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
   gulp.watch(['*.html'], ['htmlBuild']);
-  gulp.watch("scss/*.scss", ['cssBuild']);
+  gulp.watch(['scss/*.scss'], ['cssBuild']);
 });
 
-gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function() {
-  browserSync.reload();
-});
-
-gulp.task('bowerBuild', ['bower'], function() {
-  browserSync.reload();
-});
-
+//BUILDS
 gulp.task('htmlBuild', function() {
   browserSync.reload();
 });
 
-gulp.task('cssBuild', ['build'], function() {
-    return gulp.src('scss/*.scss')
+gulp.task('jsBuild', ['jsBrowserify'], function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
+});
+
+gulp.task('cssBuild', function() {
+  return gulp.src('scss/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./build/css'))
     .pipe(browserSync.stream());
-});
+  });
--- js/repos.js
+++ js/repos.js
@@ -3,7 +3,9 @@ var githubKey = require('./../.env').githubKey;
 exports.repos = function(username){
   $.get('https://api.github.com/users/' + username + '?access_token=' + githubKey).then(function(response) {
     console.log(response);
-    $('#results').append("Your username is " + username + "<br>You're following: " + response.following);
-    console.log(response);
+    if (response.location === null) {
+      response.location = "";
+    }
+    $('#results').append('<img src="' + response.avatar_url + '" width="200"><br>Username: ' + username + '<h4>' + response.location + '</h4><a href="' + response.followers_url + '"># of people who follow ' + username + ': ' + response.followers + '</a><br><a href="' + response.following_url + '"># of people who ' + username + ' follows: ' + response.following + '</a><p>Account Created: ' + response.created_at + '</p><a href="' + response.repos_url + '">List of Public Repos');
   });
 };
