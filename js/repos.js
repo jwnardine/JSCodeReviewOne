96905c70c122da830f9d11d7031c5ae8f00d0c87
jwnardine <jwnardine@gmail.com>
2016-03-21 23:34:26 -0700

    clean margins on user.js

--- js/repos.js
+++ js/repos.js
@@ -2,12 +2,11 @@ var githubKey = require('./../.env').githubKey;
 
 exports.getRepos = function(username){
     $.get('https://api.github.com/users/' + username + '/repos?access_token=' + githubKey).then(function(response){
-      console.log(response);
+        console.log(response);
 
-      for(var i = 0; i < response.length; i++)
-      {
-        $('#repoList').append('<li>Name: ' + response[i].name + '</li>');
-        $('#repoList').append('<li>Link: <a href="' + response[i].html_url + '">' + response[i].html_url + '</li><br>');
-      }
+        for(var i = 0; i < response.length; i++) {
+            $('#repoList').append('<li>Name: ' + response[i].name + '</li>');
+            $('#repoList').append('<li>Link: <a href="' + response[i].html_url + '">' + response[i].html_url + '</li><br>');
+        }
     });
 };
--- js/user.js
+++ js/user.js
@@ -1,11 +1,11 @@
 var githubKey = require('./../.env').githubKey;
 
 exports.getUser = function(username){
-  $.get('https://api.github.com/users/' + username + '?access_token=' + githubKey).then(function(response) {
-    console.log(response);
-    if (response.location === null) {
-      response.location = "";
-    }
+    $.get('https://api.github.com/users/' + username + '?access_token=' + githubKey).then(function(response) {
+        console.log(response);
+        if (response.location === null) {
+            response.location = "";
+        }
     $('#profile').append('<br><img class="img-rounded" src="' + response.avatar_url + '" width="200"><br>Username: ' + username + '<h4>' + response.location + '</h4><a href="' + response.followers_url + '"># of people who follow ' + username + ': ' + response.followers + '</a><br><a href="' + response.following_url + '"># of people who ' + username + ' follows: ' + response.following + '</a><p>Account Created: ' + response.created_at + '</p>Public repos: ' + response.public_repos + '<br><hr>');
   });
 };
